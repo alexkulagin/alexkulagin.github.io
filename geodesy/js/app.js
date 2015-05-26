f20a88d249a8a664882667df3f9736004b4fae7f
alexkulagin <evernote@alexkulagin.com>
2015-05-27 02:00:25 +0300

    update

--- geodesy/js/app.js
+++ geodesy/js/app.js
@@ -81,6 +81,19 @@
         }
 
     jQuery && jQuery(function($) {
+
+        // mobile test
+        var isMobile = { 
+            Android: function() { return navigator.userAgent.match(/Android/i); }, 
+            BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); }, 
+            iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, 
+            Opera: function() { return navigator.userAgent.match(/Opera Mini/i); }, 
+            Windows: function() { return navigator.userAgent.match(/IEMobile/i); }, 
+            any: function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());} 
+        };
+
+        if (isMobile) alert("mobile");
+
         splash = $("#splash-container");
         $(window).resize(function(){
             rf();
