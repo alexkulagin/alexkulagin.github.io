37db5fdede9b76de447ce67890ee50859f838642
Alex Kulagin <alexkulagin@users.noreply.github.com>
2017-08-26 15:12:28 +0300

    update

--- axure-toolkit/files/пример_общения_вложенных_фреймов/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(s,t,u,v,w,_(x,y,z,A),B,null,C,v,D,v,E,F,G,null,H,I,J,K,L,M,N,I),O,_(),P,_(),Q,_(R,[_(S,T,U,V,W,X,n,Y,Z,Y,ba,bb,r,_(bc,_(bd,be,bf,bg)),O,_(),bh,_(),bi,_(bj,k,b,bk,bl,bb))])),bm,_(),bn,_(bo,_(bp,bq)));}; 
-var b="url",c="пример_общения_вложенных_фреймов.html",d="generationDate",e=new Date(1503746734620.53),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="GV_Version",k="page",l="packageId",m="5428c4a2080c4c90bf62fb899d870232",n="type",o="Axure:Page",p="name",q="Пример общения вложенных фреймов",r="style",s="baseStyle",t="627587b6038d43cca051c114ac41ad32",u="pageAlignment",v="near",w="fill",x="fillType",y="solid",z="color",A=0xFFFBFBFB,B="image",C="imageHorizontalAlignment",D="imageVerticalAlignment",E="imageRepeat",F="auto",G="favicon",H="sketchFactor",I="0",J="colorStyle",K="appliedColor",L="fontName",M="Applied Font",N="borderWidth",O="adaptiveStyles",P="interactionMap",Q="diagram",R="objects",S="id",T="77b52f0ddf2a46efa89559f57c7754e6",U="label",V="nested frame a",W="friendlyType",X="Inline Frame",Y="inlineFrame",Z="styleType",ba="visible",bb=true,bc="size",bd="width",be=672,bf="height",bg=416,bh="imageOverrides",bi="target",bj="targetType",bk="вложенный_фрейм_a.html",bl="includeVariables",bm="masters",bn="objectPaths",bo="77b52f0ddf2a46efa89559f57c7754e6",bp="scriptId",bq="u1104";
-return _creator();
-})());
